889a5ab4d97b65d73d73f43c255d80a82434c28f
Basilisk <Basilisk@LAPTOP-5N98T2A6>
2021-08-08 10:09:15 +0530

    Initial check in

---
+++ parameters.m
@@ -0,0 +1,18 @@
+clear 
+
+R_s = 13.2;         % Resistance of stator
+R_r = 11.78;        % Resistance of rotor
+L_s = 0.42;         % Inductance of stator
+L_r = 0.42;         % Inductance of rotor
+L_m = 0.4;          % Mutual inductance
+T_p = 0.0465;       % Pole pitch
+M = 4.775;          % Mass of moving part
+P = 4;              % number of pole pairs
+B = 53;             % viscous friction
+Lmbda_r = 0.056;    %
+
+% L_ls, L_lr leakage inductances of stator and rotor
+% W_e   primary, frequency
+% W_r   secondary frequency 
+% W_sl  slip electrical frequency
+
